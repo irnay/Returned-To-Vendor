eb4d3d9987f504f65257da244332c576bec72568
irnay <59512861+irnay@users.noreply.github.com>
2020-01-05 01:42:12 +0700

    Update F_Splash.vb

--- F_Splash.vb
+++ F_Splash.vb
@@ -1,5 +1,5 @@
 ﻿Public Class F_Splash
     Private Sub F_Splash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        label2.text = "Irna Yanti"
+        label2.text = "gak tau"
     End Sub
 End Class
